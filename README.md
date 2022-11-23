e7b1c69f961dd40ec1469f577518c24ed8833f16
Krypto1337 <118254306+Krypto1337@users.noreply.github.com>
2022-11-23 14:42:18 +0100

    Updated readme, added Webpack config

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # WebpackTemplate
-Webpack config for every single project.
-___
 
-My small webpack config made for every project
+Webpack config for every single project.
+
+---
+
+## How to use?
+
+<!-- Merge Project-b into Project-a -->
+
+1. Clone this repo anywhere on your PC
+2. CD path/to/project-a
+3. git fetch project-b --tags
+4. git merge --allow-unrelated-histories project-b/master <!-- or whichever branch you want to merge -->
+5. git remote remove project-b
---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Tic Tac Toe",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
